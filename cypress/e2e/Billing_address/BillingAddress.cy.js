2f59a0cc7b81a786faaa41534a344b17bbe99443
Mounika9895 <chowdharymouni9895@gmail.com>
2023-12-03 13:27:18 +0530

    billingaddress.cy.js

--- cypress/e2e/Billing_address/BillingAddress.cy.js
+++ cypress/e2e/Billing_address/BillingAddress.cy.js
@@ -1,18 +1,18 @@
 describe('Fill and Submit Form Multiple Times', () => {
-
+  it("Should be able to submit a successful submission via contact us form", () => {
   cy.visit('https://student.infinitylearn.com/signin?page=signin');
   cy.get('.UNFAPP-phncuntry-drpdwn.ng-pristine .UNFAPP-phncuntry-fldgrp').type('6361415008');
   cy.get('.UNFAPP-txt-link.ng-star-inserted').click();
   cy.get('input[placeholder="Password"]').type('Pass123');
   cy.get('button[type="button"]').click();
-  
+  });
 
     for (let i = 0; i < 500; i++) {
       it(`should fill and submit form - iteration ${i + 1}`, () => {
        
       
         // // Fill in the first name
-         cy.get('#firstName').type('John');
+        // cy.get('#firstName').type('John');
   
         // // Fill in the last name
         // cy.get('#lastName').type('Doe');
