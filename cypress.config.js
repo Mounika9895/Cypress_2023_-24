40b1e41390d58904fc6f49ee3b0962cd4035e4eb
Mounika9895 <chowdharymouni9895@gmail.com>
2023-12-03 12:30:21 +0530

    iframe.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      chromeWebSecurity : false;
+      experimentalSessionAndOrigin :true;
+      defaultCommandTimeout : 10000;
+      
     },
   },
 });
