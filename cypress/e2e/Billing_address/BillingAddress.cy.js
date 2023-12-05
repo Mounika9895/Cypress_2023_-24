3ef5df5926fb2b039590d517ccd3c1c34a5ec3f2
Mounika9895 <chowdharymouni9895@gmail.com>
2023-12-05 18:49:38 +0530

    billupdate

--- cypress/e2e/Billing_address/BillingAddress.cy.js
+++ cypress/e2e/Billing_address/BillingAddress.cy.js
@@ -12,6 +12,7 @@ describe('Fill and Submit Form Multiple Times', () => {
   cy.get('.SUBCRP-blue-btn.full').should('contain.text', 'Buy Now')
   .first().click();
   cy.wait(2000)
+  cy.get('.SUBCRP-blue-btn').should('have.length', 6).last().click();
   cy.get('.SUBCRP-blue-btn').should('contain.text', 'Proceed').last().click();
 
 
