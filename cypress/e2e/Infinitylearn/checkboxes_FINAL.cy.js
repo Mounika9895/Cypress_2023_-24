bc29751e65f88b8bb63c2d2c45d34553d5492140
Mounika9895 <chowdharymouni9895@gmail.com>
2023-12-16 22:51:57 +0530

    updatedcheckbox

--- cypress/e2e/Infinitylearn/checkboxes_FINAL.cy.js
+++ cypress/e2e/Infinitylearn/checkboxes_FINAL.cy.js
@@ -2,8 +2,8 @@
 
 describe("Verify checkboxes via webdriveruni", () => {
     it("Check and validate checkbox", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+        cy.visit("http://www.webdriveruniversity.com");
+        //cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
 
         //cy.get('#checkboxes > :nth-child(1) > input').check()
         //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
