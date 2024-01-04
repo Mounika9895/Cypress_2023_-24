5760243c7aee30bf1ea4b2e6c42373a7a2937561
Mounika9895 <chowdharymouni9895@gmail.com>
2024-01-04 23:08:19 +0530

    scroll.cy.js

---
+++ cypress/e2e/Praticebasiccodes/mouse-actions.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe("Test mouse actions", () => {
+    it("Scroll element into view", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({force:true})
+    });
+}) 
